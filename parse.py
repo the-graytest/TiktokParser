f257fc69e119c4545dbd9f7ca2cd4801a770e791
Gray Tambling <graytambling@gmail.com>
2025-01-12 19:52:26 -0500

    Create parse.py

---
+++ parse.py
@@ -0,0 +1,34 @@
+import re
+import asyncio
+import json
+
+# Usage python parse.py >> results.txt
+
+def extract_links(path):
+    links = set()
+    tiktok_pattern = re.compile(r'https://www\.tiktok\.com/@\S+/video/\d+')
+
+    with open(path) as f:
+        lines = f.readlines()
+        columns = []
+
+        for line in lines:
+            matches = tiktok_pattern.findall(line)
+            if len(matches) > 0:
+
+                link = matches[0]
+                if link[-1] == ")":
+                   link = link[:-1]
+                links = set([link]) | set(links)
+    return links
+
+
+
+docx_path = 'recipes.txt' # Change this to the path for your .txt file
+
+
+
+# Extract and print the links
+final_links = set(extract_links(docx_path))
+for link in final_links:
+    print (link)
